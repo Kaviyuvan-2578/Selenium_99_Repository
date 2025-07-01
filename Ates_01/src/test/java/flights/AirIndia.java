a542c27e911088932cbbcbecf34fddd5a0265583
HP <HP@DESKTOP-382NOLR>
2025-07-01 13:17:39 +0530

    flights commit

---
+++ Ates_01/src/test/java/flights/AirIndia.java
@@ -0,0 +1,22 @@
+package flights;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AirIndia {
+	
+	@Test
+	public void demo() {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		driver.get("https://www.airindia.com/");
+	}
+	
+
+}
